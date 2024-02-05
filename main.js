803f3a12be2288360d523e0f27308472bffaa035
AhmadKawasmi <96664593+AhmadKawasmi@users.noreply.github.com>
2024-02-05 23:48:40 +0200

    almost done

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const samsungTv = {
     storeName:"Samsung",
     address:"XXXXX",
     city:"Tel-Aviv",
-    price:"2499",
+    price:2499,
     storeUrl:"",
-    rating:"4/5"
+    rating:"3/5"
 }
 
 const lgTv = {
@@ -15,7 +15,7 @@ const lgTv = {
     storeName:"LG",
     address:"XXXXX",
     city:"Jerusalem",
-    price:"6798",
+    price:6798,
     storeUrl:"",
     rating:"5/5"
 }
@@ -26,9 +26,9 @@ const haierTv = {
     storeName:"Haier",
     address:"XXXXX",
     city:"Haifa",
-    price:"1699",
+    price:1699,
     storeUrl:"",
-    rating:"2/5"
+    rating:"4/5"
 }
 
 const toshibaTv = {
@@ -37,33 +37,94 @@ const toshibaTv = {
     storeName:"Toshiba",
     address:"XXXXX",
     city:"Natanya",
-    price:"2222",
+    price:2222,
     storeUrl:"",
-    rating:"3/5"
+    rating:"1/5"
 }
-
 
 let tableArray = [samsungTv,lgTv,haierTv,toshibaTv]
 
+//2- 
+
+const rate = document.querySelectorAll(".rating")
+
+
+rate.forEach(row => {
+    const rating = row.textContent
+    const fullRow = row.closest('tr')
+    console.log(fullRow)
+    if (rating >= 4) {
+        row.classList = "greenBackground"
+    }else if (rating < 2) {
+        row.classList = "redBackground"
+    }
+})
+
+
+// 3-  function that receives an array of objects and
+// return the average price of all the rows.
 
 function createSumArray(array) {
-    let sumArray = []
+    let pricesArray = []
     let tvPrice ;
         for (let i = 0; i < array.length; i++) {
             if (array[i].price > 0) {
                 tvPrice = array[i].price
-                sumArray.push(tvPrice)
+                pricesArray.push(tvPrice)
             }
             
         }
-        return sumArray
+
+        let sum = 0 ;
+        pricesArray.forEach(itemPrice => {
+            sum += itemPrice / pricesArray.length
+           
+        })
+        console.log(sum)
+        return sum,pricesArray
 }
 
-console.log(createSumArray(tableArray));
+console.log(createSumArray(tableArray))
 
-function calculateSum(array) {
-    let sum ;
-    array.forEach(n =>{
+// 4-function that receives an array of objects
+// return the id of the row, only if the price is
+// the lowest and the rating is 4/5 or 5/5.
+
+function bestOption(array) {
+    let result 
+
+    let pricesArray = []
+    let tvPrice ;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].price > 0) {
+                tvPrice = array[i].price
+                pricesArray.push(tvPrice)
+            }
+            
+        }
+
+    let minPrice = Math.min(...pricesArray)
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].price === minPrice) {
+            if(array[i].rating === "4/5" || array[i].rating === "5/5" ){
+                result = array[i]
+            }
+            
+        }
         
-    })
+    }
+    return result
 }
+
+console.log(bestOption(tableArray))
+
+//5- remove row when clicked on delete btn 
+
+const deleteButtons = document.querySelectorAll(".deleteBtn");
+
+deleteButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        button.closest('tr').remove();
+    });
+});
